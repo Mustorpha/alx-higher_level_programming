378fe22aec3f075efb65a1cf6a169031dcad762f
mustorpha <mustorphajamiu@gmail.com>
2023-10-03 08:25:50 -0700

    Tasks Bild 2.1

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,22 @@
 #include "lists.h"
 
 /**
- * check_cycle - check for loop in LL
- * @list: head of linked list
- * Return: 1 if cycled, 0 if not
+ * check_cycle - check for circular linked list
+ * @list: head of the linked list
+ * Return: 1 if circular, 0 if not
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	listint_t *current = NULL;
 
-	if (!list)
+	current = list;
+
+	while (current)
 	{
-		return (0);
-	}
-	slow = list;
-	fast = list->next;
-	while (fast && slow && fast->next)
-	{
-		if (slow == fast)
-		{
+		if (current->next = list)
 			return (1);
-		}
-		slow = slow->next;
-		fast = fast->next->next;
+		current = current->next;
 	}
+
 	return (0);
 }
