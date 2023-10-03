0a00aa655dd978de43d0093664fb2ba6063fdfc0
mustorpha <mustorphajamiu@gmail.com>
2023-10-03 09:46:06 -0700

    Tasks Build 3.1

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,21 +1,24 @@
 #include "lists.h"
 
 /**
- * check_cycle - check for circular linked list
- * @list: head of the linked list
- * Return: 1 if circular, 0 if not
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: pointer to linked list head
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *current = NULL;
+	listint_t *current = list;
+	listint_t *lookup = list;
 
-	current = list;
+	if (!list)
+		return (0);
 
-	while (current)
+	while (current && lookup && lookup->next)
 	{
-		if (current->next == list)
-			return (1);
 		current = current->next;
+		lookup = lookup->next->next;
+		if (current == lookup)
+			return (1);
 	}
 
 	return (0);
