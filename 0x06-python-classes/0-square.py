1624112c7e9a539e5f56147405dd544f4bfb7ab2
mustorpha <mustorphajamiu@gmail.com>
2023-10-24 15:05:45 -0700

    google documentation style

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!usr/bin/python3
 # _*_ coding: utf-8 _*_
 
-'''' An Empty Class definition '''
+"""An Empty Class definition"""
 
 class Square():
-    ''' A square class  '''
+    """A square class"""
     pass
