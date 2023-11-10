029e70b1d95cae0c473d5038468535dcdbb9d8a0
~thebigmuse~ <mustorphajamiu@gmail.com>
2023-11-10 07:44:09 +0000

    Added the Rectangle class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,12 +2,21 @@
 
 """ base class definition  """
 
-class Base():
-    """ Base class  """
+class Base:
+    """ Base class
+
+    Class attributes:
+        __nb_objects (int): number of objects created
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ initializes the base class attributes  """
+        """ initializes the base class attributes
+
+        Args:
+            id (int): unique id number of an object
+        """
 
         if id:
             self.id = id
